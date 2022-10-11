8ef4c1caf124ac10527f583393418ac3241f3ec2
rifatptk <rifatptk@gmail.com>
2022-10-11 15:18:26 +0600

    Fixed sidebar issue

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,9 +7,9 @@ const Sidebar = ({ isExpanded, setSidebarExpanded }) => {
   return (
     <div className="sidebar border-r bg-white h-[100vh] relative">
       {/* sidebar toggle */}
-      <div className="absolute -right-[14px] top-9 bg-white cursor-pointer rounded-full shadow-md">
+      <div className="absolute -right-[12px] top-9 bg-white cursor-pointer rounded-full shadow-md">
         <BsArrowRightCircle
-          size={28}
+          size={24}
           onClick={() => setSidebarExpanded(!isExpanded)}
           className={`text-theme-cyan pointer transition-all hover:text-theme-blue ${
             isExpanded ? 'rotate-180' : ''
